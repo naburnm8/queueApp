eb9f44b521f72f479641950ada5720a0e2fce6e7
naburnm8 <rtender.artem@gmail.com>
2026-03-10 22:22:39 +0300

    add network module to Koin initialization

--- app/src/main/java/ru/naburnm8/queueapp/QueueAppApplication.kt
+++ app/src/main/java/ru/naburnm8/queueapp/QueueAppApplication.kt
@@ -5,6 +5,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
 import org.koin.core.logger.Level
+import ru.naburnm8.queueapp.modules.networkModule
 import ru.naburnm8.queueapp.modules.repositoryModule
 import ru.naburnm8.queueapp.modules.viewmodelModule
 
@@ -15,7 +16,7 @@ class QueueAppApplication : Application(){
         startKoin {
             androidLogger(Level.DEBUG)
             androidContext(this@QueueAppApplication)
-            modules(listOf(repositoryModule, viewmodelModule))
+            modules(listOf(repositoryModule, viewmodelModule, networkModule))
         }
     }
 }
